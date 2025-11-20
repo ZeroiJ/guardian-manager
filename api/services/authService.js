28f5fb2ea07ae855c0e1c966930013de9215ec27
ZeroiJ <sujalbirwadkar19@gmail.com>
2025-11-20 16:17:41 +0530

    feat: Add Bungie OAuth authentication service for token exchange and refresh.

--- api/services/authService.js
+++ api/services/authService.js
@@ -4,12 +4,17 @@ const qs = require('qs');
 const TOKEN_URL = 'https://www.bungie.net/Platform/App/OAuth/token/';
 
 async function getTokensFromCode(code) {
-    const data = qs.stringify({
+    const body = {
         grant_type: 'authorization_code',
         code: code,
-        client_id: process.env.BUNGIE_CLIENT_ID,
-        client_secret: process.env.BUNGIE_CLIENT_SECRET
-    });
+        client_id: process.env.BUNGIE_CLIENT_ID
+    };
+
+    if (process.env.BUNGIE_CLIENT_SECRET) {
+        body.client_secret = process.env.BUNGIE_CLIENT_SECRET;
+    }
+
+    const data = qs.stringify(body);
 
     try {
         const response = await axios.post(TOKEN_URL, data, {
@@ -25,12 +30,17 @@ async function getTokensFromCode(code) {
 }
 
 async function refreshAccessToken(refreshToken) {
-    const data = qs.stringify({
+    const body = {
         grant_type: 'refresh_token',
         refresh_token: refreshToken,
-        client_id: process.env.BUNGIE_CLIENT_ID,
-        client_secret: process.env.BUNGIE_CLIENT_SECRET
-    });
+        client_id: process.env.BUNGIE_CLIENT_ID
+    };
+
+    if (process.env.BUNGIE_CLIENT_SECRET) {
+        body.client_secret = process.env.BUNGIE_CLIENT_SECRET;
+    }
+
+    const data = qs.stringify(body);
 
     try {
         const response = await axios.post(TOKEN_URL, data, {
