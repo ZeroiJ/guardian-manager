8c3d97a685b9a1722f51b037893d771d4304402f
ZeroiJ <sujalbirwadkar19@gmail.com>
2025-12-12 12:58:29 +0530

    Fix: Make client_secret optional for Public OAuth clients

--- functions/services/authService.js
+++ functions/services/authService.js
@@ -1,10 +1,15 @@
 export async function getTokensFromCode(code, env) {
-    const body = new URLSearchParams({
+    const params = {
         grant_type: 'authorization_code',
         code: code,
-        client_id: env.BUNGIE_CLIENT_ID,
-        client_secret: env.BUNGIE_CLIENT_SECRET
-    });
+        client_id: env.BUNGIE_CLIENT_ID
+    };
+
+    if (env.BUNGIE_CLIENT_SECRET) {
+        params.client_secret = env.BUNGIE_CLIENT_SECRET;
+    }
+
+    const body = new URLSearchParams(params);
 
     const response = await fetch('https://www.bungie.net/Platform/App/OAuth/token/', {
         method: 'POST',
@@ -23,12 +28,17 @@ export async function getTokensFromCode(code, env) {
 }
 
 export async function refreshAccessToken(refreshToken, env) {
-    const body = new URLSearchParams({
+    const params = {
         grant_type: 'refresh_token',
         refresh_token: refreshToken,
-        client_id: env.BUNGIE_CLIENT_ID,
-        client_secret: env.BUNGIE_CLIENT_SECRET
-    });
+        client_id: env.BUNGIE_CLIENT_ID
+    };
+
+    if (env.BUNGIE_CLIENT_SECRET) {
+        params.client_secret = env.BUNGIE_CLIENT_SECRET;
+    }
+
+    const body = new URLSearchParams(params);
 
     const response = await fetch('https://www.bungie.net/Platform/App/OAuth/token/', {
         method: 'POST',
